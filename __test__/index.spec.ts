6111fdf43b7c5859aa09eeda62c1ec9d0ffb4785
Coooookies <admin@mitay.net>
2025-03-09 20:19:00 +0800

    chore: update test

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,13 +1,13 @@
 import test from "ava"
-import { getCurrentSession, getAllSessions, getSessionById } from "../binding"
+import { getCurrentSession, getSessions, getSessionById } from "../binding"
 
 test("getCurrentSession() should return MediaInfo | null", (t) => {
   const session = getCurrentSession()
   t.true(session === null || "sourceAppId" in session)
 })
 
-test("getAllSessions() should return MediaInfo[]", (t) => {
-  const sessions = getAllSessions()
+test("getSessions() should return MediaInfo[]", (t) => {
+  const sessions = getSessions()
   t.true(Array.isArray(sessions))
 })
 
